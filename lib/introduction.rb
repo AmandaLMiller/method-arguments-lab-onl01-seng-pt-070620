106ffaeded38fa9c822dd3238d68d40b9735fe7a
Amanda L Miller <amanda.l.miller1989@gmail.com>
2020-07-14 00:58:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,6 @@
 #introduction_with_language
 def introduction(name)
+ puts "Hi, my name is #{name}."
 end
 def introduction_with_language(name, language)
   puts "Hi my name is #{name} and I am learning to 
